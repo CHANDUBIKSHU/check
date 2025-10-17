2be87b71dbf289ade21aebdac7ef82832b52104e
CHANDUBIKSHU <bcfriends143@gmail.com>
2025-10-17 15:21:30 +0530

    Update API endpoint in Contactform component

--- frontend/src/components/Contactform.jsx
+++ frontend/src/components/Contactform.jsx
@@ -21,7 +21,7 @@ export default function ContactForm() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://check-0f2d.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
